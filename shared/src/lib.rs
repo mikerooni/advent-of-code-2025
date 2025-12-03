04e122dd4e8dd523ea3824f605f353f2a6abbcdf
Mikerooni <139889766+mikerooni@users.noreply.github.com>
2025-12-03 12:33:22 +0100

    add stdin reader

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::io::stdin;
 
 pub fn print_program_header(day: u8, problem_name: &str) {
     println!("+-----------------------------------------------+");
@@ -13,4 +14,21 @@ pub fn read_data(day: u8) -> String {
     let data = fs::read(format!("./data/day{}.txt", day)).expect("Cannot open the input file.");
 
     String::from_utf8(data).expect("Input file cannot be read as UTF-8")
+}
+
+pub fn read_data_stdin() -> String {
+    println!("Write \"####END####\" to end the input.");
+
+    let mut input = String::new();
+
+    loop {
+        stdin().read_line( & mut input ).unwrap();
+        let input_line= input.lines().last().unwrap();
+
+        if input_line == "####END####" {
+            break;
+        }
+    }
+
+    input
 }
